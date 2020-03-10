ae690d19214678f1d1bf1230dc646c743b5108d7
Andycruz008 <Andycruz008@gmail.com>
2020-03-10 08:57:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,21 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+
+    post '/team' do
+    @team_name = params["name"] 
+    @coach = params["coach"]
+    @point = params["pg"]
+    @shoot = params["sg"]
+    @power = params["pf"]
+    @small = params["sf"]
+    @center = params["c"]
+    erb :team
+
+    end
+
 end
